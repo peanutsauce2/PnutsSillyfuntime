3bb41b370689726f287959c4631a8b13bd424c6c
peanutsauce2 <stinkyguys542@gmail.com>
2026-08-12 18:53:06 -0700

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,28 @@
+
+let songs = [
+  {
+        name: "Breath - NAOKI and Arc SYS",
+        artist: "NAOKI and Arc Sys",
+        img: "Breath1",
+        audio: "Breath1"
+  },
+  {
+        name : "Extras - Molly Daisy Scarpine and Daisuke Ishiwatari or NAOKI idk ",
+        artist : "Molly Daisy Scarpine and Daisuke Ishiwatari or NAOKI idk",
+        img : "Extras3",
+        audio : "Extras3"
+  },
+  {
+        name: "Symphony - NAOKI and Arc SYS",
+        artist: "NAOKI and Arc Sys",
+        img: "Symphony4",
+        audio: "Symphony4"
+  },
+  {
+        name: "CARPE DIEM- NAOKI and Arc SYS",
+        artist: "NAOKI and Arc Sys",
+        img: "CARPE DIEM2",
+        audio: "CARPE DIEM2"
+  },
+
+]
